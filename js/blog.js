78ae377c6d375f5e0b3afd576f05cacee44d0cd4
mrcd_noah <mrcd.noah@gmail.com>
2025-04-18 13:23:17 +0800

    get timezone from api

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+
+// Define the API URL
+const apiUrl = 'https://timeapi.io/api/time/current/zone?timeZone=Asia%2FSingapore';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
