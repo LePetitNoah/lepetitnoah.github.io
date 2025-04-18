ff6a97c3af571df7b0d47a64297dd03349a657b6
mrcd_noah <mrcd.noah@gmail.com>
2025-04-18 17:03:54 +0800

    debuggage

--- js/blog.js
+++ js/blog.js
@@ -6,7 +6,6 @@ window.addEventListener("load", async () => {
         fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Seri%20Iskandar?unitGroup=metric&include=current&key=4ULXBJL7H77CEEB28PQE8N8F2&contentType=json")
         .then(response => response.json())
         .then(json => {
-            console.log(json);
             document.body.onload = changeWeather(json);
         });
 
@@ -20,11 +19,16 @@ window.addEventListener("load", async () => {
 });
 
 function changeTime(elem){
+
+    console.log("Change time called");
+
     const time = document.getElementById("time");
     time.innerHTML = "Heure actuelle en Malaisie : " + elem.time;
 }
 
 function changeWeather(elems){
+
+    console.log("Change weather called");
 
     const daysElement = [];
     for(let i=0; i<3; i++){
