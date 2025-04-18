73d24863f789c3f9175af80730b7d68e7d02e01e
mrcd_noah <mrcd.noah@gmail.com>
2025-04-18 19:03:07 +0800

    style du blog

--- js/blog.js
+++ js/blog.js
@@ -1,29 +1,31 @@
 window.addEventListener("load", async () => {
 
-    let loader = document.getElementById("loader");
-    loader.addEventListener("click", () => {
+    setInterval(fetchTime, 10000);
+    fetchWeather();
+});
 
-        fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Seri%20Iskandar?unitGroup=metric&include=current&key=4ULXBJL7H77CEEB28PQE8N8F2&contentType=json")
-        .then(response => response.json())
-        .then(json => {
-            document.body.onload = changeWeather(json);
-        });
-
-        fetch("https://timeapi.io/api/time/current/zone?timeZone=Asia%2FSingapore")
+function fetchTime(){
+    fetch("https://timeapi.io/api/time/current/zone?timeZone=Asia%2FSingapore")
         .then(response => response.json())
         .then(json => {
             console.log(json);
             document.body.onload = changeTime(json);
-        });
     });
-});
+}
 
+function fetchWeather(){
+    fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Seri%20Iskandar?unitGroup=metric&include=current&key=4ULXBJL7H77CEEB28PQE8N8F2&contentType=json")
+        .then(response => response.json())
+        .then(json => {
+            document.body.onload = changeWeather(json);
+    });
+}
 function changeTime(elem){
 
     console.log("Change time called");
 
     const time = document.getElementById("time");
-    time.innerHTML = "Heure actuelle en Malaisie : " + elem.time;
+    time.innerHTML = elem.time;
 }
 
 function changeWeather(elems){
@@ -39,7 +41,7 @@ function changeWeather(elems){
         days[i] = elems.days[i];
     }
 
-    day1.innerHTML = "Température actuelle en Malaisie : " + days[0].feelslike;
-    day2.innerHTML = "Température actuelle en Malaisie : " + days[1].feelslike;
-    day3.innerHTML = "Température actuelle en Malaisie : " + days[2].feelslike;
+    day1.innerHTML = "Température actuelle en Malaisie : " + days[0].feelslike + "C°";
+    day2.innerHTML = "Température prévue demain en Malaisie : " + days[1].feelslike + "C°";
+    day3.innerHTML = "Température prévue après-demain en Malaisie : " + days[2].feelslike + "C°";
 }
