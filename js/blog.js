0eb7e5f949c57f7c38f050eb92b9154bb2ae003f
mrcd_noah <mrcd.noah@gmail.com>
2025-04-18 16:52:13 +0800

    affichage température

--- js/blog.js
+++ js/blog.js
@@ -7,19 +7,36 @@ window.addEventListener("load", async () => {
         .then(response => response.json())
         .then(json => {
             console.log(json);
+            document.body.onload = changeWeather(json);
         });
-        /*
+
         fetch("https://timeapi.io/api/time/current/zone?timeZone=Asia%2FSingapore")
         .then(response => response.json())
         .then(json => {
             console.log(json);
             document.body.onload = changeTime(json);
         });
-        */
     });
 });
 
 function changeTime(elem){
     const time = document.getElementById("time");
     time.innerHTML = "Heure actuelle en Malaisie : " + elem.time;
+}
+
+function changeWeather(elems){
+
+    const daysElement = [];
+    for(let i=0; i<3; i++){
+        daysElement[i] = document.getElementById("day" + i+1);
+    }
+
+    const days = [];
+    for(let i=0; i<3; i++){
+        days[i] = elems.days[i];
+    }
+
+    for(let i=0; i<daysElement.length; i++){
+        daysElement[i].innerHTML = "Températures actuelles en Malaisie : " + days[i].feelslike;
+    }
 }
